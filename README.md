def801e1494ff76412672ecdcb301b0e0e2e72b0
davidwang456 <davidwang456@sina.com>
2021-06-26 01:57:47 +0800

    bug request

--- README.md
+++ README.md
@@ -9,18 +9,18 @@ version:5.7.3-m13
 
 ## Steps to reproduce
 1.create session1 to query all students with mybatis
-2.use session1 query all students with mybatis
-3.use native jdbc update one student 
+2.use native jdbc update one student 
+3.use session1 query all students with mybatis
 4.create session 2 to query all student with mybatis
 ## Expected result
 1.get all students infos from db;
-2.get all students infos from localCache;
-3.one student was updated success in db; 
+2.one student was updated success in db; 
+3.get all students infos from localCache;
 4.get modified students info from db;
 ## Actual result
 1.get all students infos from db;  ok!
-2.get all students infos from localCache; ok!
-3.one student was updated success in db; ok!
+2.one student was updated success in db; ok!
+3.get all students infos from localCache; ok!
 4.get modified students info from db; no! get not modified students from localCache
 
 ## more infomation:
---
+++ mysql-schema.sql
@@ -0,0 +1,20 @@
+CREATE database davidwang456;
+use davidwang456;
+
+DROP TABLE IF EXISTS  student;
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (1, 'wang1', 'david1', 21);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (2, 'wang2', 'david2', 22);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (3, 'wang3', 'david3', 23);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (4, 'wang4', 'david4', 24);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (5, 'wang5', 'david5', 25);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (6, 'wang6', 'david6', 26);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (7, 'wang7', 'david7', 27);
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `age`) VALUES (8, 'wang8', 'david8', 28);
